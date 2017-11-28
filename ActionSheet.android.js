0c27c17a8728b56b58b081280252192fa5b460ec
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2017-11-28 18:40:11 -0200

    add scrollview to actionsheet

--- ActionSheet.android.js
+++ ActionSheet.android.js
@@ -1,5 +1,4 @@
 // @flow
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
@@ -16,6 +15,7 @@ import {
   TouchableNativeFeedback,
   TouchableWithoutFeedback,
   View,
+  ScrollView,
 } from 'react-native';
 
 type ActionSheetOptions = {
@@ -119,7 +119,9 @@ class ActionGroup extends React.Component {
 
     return (
       <View style={styles.groupContainer}>
-        {optionViews}
+        <ScrollView>
+          {optionViews}
+        </ScrollView>
       </View>
     );
   }
